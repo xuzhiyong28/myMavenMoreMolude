7a2765f85cca708dd91b1ded55167482d3d3ae6d
xuzhiyong28 <jip0574@qq.com>
2018-12-06 17:34:43 +0800

    xzy_branch_001 update

--- mvmoremodule-jms/src/test/java/xzy/jodoTime/Demo1.java
+++ mvmoremodule-jms/src/test/java/xzy/jodoTime/Demo1.java
@@ -1,9 +1,12 @@
 package xzy.jodoTime;
 
 import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.junit.Test;
 
 import java.util.Date;
+import java.util.Locale;
 
 public class Demo1 {
     @Test
@@ -52,5 +55,45 @@ public class Demo1 {
         System.out.println(dateTime3);
     }
 
+    @Test
+    public void test4(){
+        //返回Property的方法：Property是DateTime中的属性，保存了一些有用的信息
+        DateTime dateTime = new DateTime(new Date());
+        //获取日期的月份，天
+        System.out.println(dateTime.monthOfYear().getAsText());
+        System.out.println(dateTime.monthOfYear().getAsString());
+        System.out.println(dateTime.dayOfMonth().getAsText());
+        System.out.println(dateTime.dayOfMonth().getAsString());
+        System.out.println(dateTime.dayOfWeek().getAsText());
+        System.out.println(dateTime.dayOfWeek().getAsString());
+    }
 
+    @Test
+    public void test5(){
+        //有时我们需要对一个DateTime的某些属性进行置0操作。比如，我想得到当天的0点时刻。那么就需要用到Property中round开头的方法（roundFloorCopy）
+        DateTime dateTime = new DateTime(new Date());
+        System.out.println(dateTime.dayOfWeek().roundCeilingCopy()); // 得到今天的0点时刻
+    }
+
+    @Test
+    public void test6(){
+        //格式化日期
+        DateTime dateTime = new DateTime(new Date());
+        System.out.println(dateTime.toString("yyyy-MM-dd hh:mm:ss"));
+        System.out.println(dateTime.toString("EEEE dd MMMM, yyyy HH:mm:ssa"));
+        System.out.println(dateTime.toString("MM/dd/yyyy HH:mm ZZZZ"));
+        System.out.println(dateTime.toString("MM/dd/yyyy HH:mm Z"));
+
+        DateTime dateTime1 = new DateTime(1456473917004L);
+        System.out.println(dateTime1.toString("yyyy-MM-dd hh:mm:ss"));
+        System.out.println(dateTime1.toString("yyyy-MM-dd hh:mm:ss", Locale.CHINESE));
+
+        //根据指定格式将字符串转成DateTime对象
+        DateTime dt2 = DateTimeFormat.forPattern( "yyyy-MM-dd HH:mm:ss" ).parseDateTime( "2012-12-26 03:27:39" );
+    }
+
+    @Test
+    public void test7(){
+
+    }
 }
