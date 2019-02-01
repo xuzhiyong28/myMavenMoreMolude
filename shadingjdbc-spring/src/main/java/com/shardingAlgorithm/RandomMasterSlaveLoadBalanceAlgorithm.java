c5ece61f28636b4e846a511081874750d527404c
xuzhiyong28 <jip0574@qq.com>
2019-02-01 17:53:19 +0800

    xzy_branch_001 update

--- shadingjdbc-spring/src/main/java/com/shardingAlgorithm/RandomMasterSlaveLoadBalanceAlgorithm.java
+++ shadingjdbc-spring/src/main/java/com/shardingAlgorithm/RandomMasterSlaveLoadBalanceAlgorithm.java
@@ -17,6 +17,7 @@ public class RandomMasterSlaveLoadBalanceAlgorithm implements MasterSlaveLoadBal
     public String getDataSource(String name, String masterDataSourceName, List<String> slaveDataSourceNames) {
         System.out.println("主库 = " + masterDataSourceName);
         System.out.println("从库 = " + slaveDataSourceNames);
+        //这里可以写从库的获取算法。例如采用轮询 或者 随机访问
         return slaveDataSourceNames.get(0);
     }
 }
--- shadingjdbc-spring/src/test/java/com/ShardingTestMasterSlaveTest.java
+++ shadingjdbc-spring/src/test/java/com/ShardingTestMasterSlaveTest.java
@@ -1,15 +1,37 @@
 package com;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:applicationContext.xml", "classpath:applicationContext-database-master-slave.xml", "classpath:applicationContext-sharding-master-slave.xml"})
 public class ShardingTestMasterSlaveTest {
     @Resource(name = "jdbcTemplate")
     private JdbcTemplate jdbcTemplate;
+
+    @Test
+    public void testCreateTable() {
+        jdbcTemplate.update("CREATE TABLE IF NOT EXISTS edge (flowtime VARCHAR(50) NOT NULL, value INT NOT NULL)");
+    }
+
+    @Test
+    public void testInsertOne(){
+        //测试一条记录多条插入
+        jdbcTemplate.update("INSERT IGNORE INTO edge(flowtime,value) VALUES ('20190525',1)");
+    }
+
+
+    @Test
+    public void query(){
+        List<Map<String, Object>> list = jdbcTemplate.queryForList("select * from edge where flowtime = '20190525'");
+        System.out.println(list);
+    }
+
 }
