525c0a926b933465b646ccf1745936d5169a3e8f
xuzhiyong28 <jip0574@qq.com>
2019-02-02 09:55:42 +0800

    xzy_branch_001 update

--- shadingjdbc-spring/src/main/java/com/shardingAlgorithm/RandomMasterSlaveLoadBalanceAlgorithm.java
+++ shadingjdbc-spring/src/main/java/com/shardingAlgorithm/RandomMasterSlaveLoadBalanceAlgorithm.java
@@ -3,11 +3,15 @@ package com.shardingAlgorithm;
 import io.shardingsphere.core.api.algorithm.masterslave.MasterSlaveLoadBalanceAlgorithm;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class RandomMasterSlaveLoadBalanceAlgorithm implements MasterSlaveLoadBalanceAlgorithm {
 
+    private static int MAX_INDEX = 100000;
+
+    private static AtomicInteger index = new AtomicInteger(0);
     /***
-     *
+     * 采用用计数器%2来轮询两台服务器上的路由
      * @param name
      * @param masterDataSourceName 主库
      * @param slaveDataSourceNames 从库列表
@@ -15,9 +19,13 @@ public class RandomMasterSlaveLoadBalanceAlgorithm implements MasterSlaveLoadBal
      */
     @Override
     public String getDataSource(String name, String masterDataSourceName, List<String> slaveDataSourceNames) {
-        System.out.println("主库 = " + masterDataSourceName);
-        System.out.println("从库 = " + slaveDataSourceNames);
-        //这里可以写从库的获取算法。例如采用轮询 或者 随机访问
-        return slaveDataSourceNames.get(0);
+        if(index.get() > MAX_INDEX){
+            synchronized (RandomMasterSlaveLoadBalanceAlgorithm.class){
+                if(index.get() > MAX_INDEX){
+                    index = new AtomicInteger(0);
+                }
+            }
+        }
+        return slaveDataSourceNames.get(index.incrementAndGet() % 2);
     }
 }
