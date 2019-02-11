e6090846deb60c93d62184bf04e3c06cf2654b58
xuzhiyong28 <jip0574@qq.com>
2019-02-11 13:52:23 +0800

    xzy_branch_001 update

--- shadingjdbc-spring/src/main/java/com/util/SpringUtils.java
+++ shadingjdbc-spring/src/main/java/com/util/SpringUtils.java
@@ -8,4 +8,9 @@ public class SpringUtils {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:applicationContext.xml","classpath:applicationContext-database.xml","classpath:applicationContext-sharding.xml");
         return applicationContext.getBean(name);
     }
+
+    public static Object getBeanMaster(String name){
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:applicationContext.xml","classpath:applicationContext-database-master-slave.xml","classpath:applicationContext-sharding-master-slave.xml");
+        return applicationContext.getBean(name);
+    }
 }
