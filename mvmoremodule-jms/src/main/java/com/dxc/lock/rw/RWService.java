e17bd344a78c93f7074a91900c7895bbdd603580
xuzhiyong28 <jip0574@qq.com>
2019-02-12 14:20:12 +0800

    xzy_branch_001 update

--- mvmoremodule-jms/src/main/java/com/dxc/lock/rw/RWService.java
+++ mvmoremodule-jms/src/main/java/com/dxc/lock/rw/RWService.java
@@ -14,7 +14,6 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 public class RWService {
     private final ReadWriteLock lock = new ReentrantReadWriteLock();
     private final Lock readLock = lock.readLock();
-
     private final Lock writeLock = lock.writeLock();
 
     public void write(){
