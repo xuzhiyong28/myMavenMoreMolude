baa202ae36051e0560ae32fdb54829945581af85
xuzhiyong28 <jip0574@qq.com>
2019-02-17 10:30:00 +0800

    添加新修改的内容

--- hadoop-demo/src/test/com/HDFSTest.java
+++ hadoop-demo/src/test/com/HDFSTest.java
@@ -2,16 +2,24 @@ package com;/**
  * Created by Administrator on 2019-02-17.
  */
 
+import org.apache.hadoop.fs.FsUrlStreamHandlerFactory;
 import org.junit.Test;
 
+import java.io.InputStream;
+import java.net.URL;
+import org.apache.hadoop.io.IOUtils;
 /**
  * @author xuzhiyong
  * @createDate 2019-02-17-10:22
  */
 public class HDFSTest {
     @Test
-    public void test1(){
-
+    public void test1() throws Exception {
+        //设置可以使用hdfs协议
+        URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory());
+        URL url = new URL("hdfs://192.168.199.128:9000/hello.txt");
+        InputStream inputStream = url.openStream();
+        IOUtils.copyBytes(inputStream,System.out,4096,true);
     }
 
 }
