d4a327676a206fae932249f63f489cef95eea919
xuzhiyong28 <jip0574@qq.com>
2019-02-17 10:23:03 +0800

    添加新修改的内容

---
+++ hadoop-demo/src/main/java/com/HDFSDemo1.java
@@ -0,0 +1,18 @@
+package com;/**
+ * Created by Administrator on 2019-02-17.
+ */
+
+/**
+ * @author xuzhiyong
+ * @createDate 2019-02-17-10:20
+ */
+public class HDFSDemo1 {
+    public static void main(String[] args){
+
+    }
+
+    public void test1(){
+
+    }
+
+}
---
+++ hadoop-demo/src/test/com/HDFSTest.java
@@ -0,0 +1,17 @@
+package com;/**
+ * Created by Administrator on 2019-02-17.
+ */
+
+import org.junit.Test;
+
+/**
+ * @author xuzhiyong
+ * @createDate 2019-02-17-10:22
+ */
+public class HDFSTest {
+    @Test
+    public void test1(){
+
+    }
+
+}
