b677e383383c9ebb5318dbc338aad7b07ff8bf42
xuzhiyong28 <jip0574@qq.com>
2019-03-11 17:13:14 +0800

    xzy_branch_001 update

--- mvmoremodule-jms/src/main/java/com/dxc/atomic/AtomicIntegerArrayTest.java
+++ mvmoremodule-jms/src/main/java/com/dxc/atomic/AtomicIntegerArrayTest.java
@@ -9,6 +9,9 @@ import java.util.concurrent.atomic.AtomicIntegerArray;
  * @author xuzhiyong
  * @createDate 2018-06-08-14:34
  * 原子的方式对values里面的值进行递增 采用 AtomicInteger方式
+ * 使用原子类有两个缺陷:
+ *  1.ABA问题：因为 CAS 需要在操作值的时候检查下值有没有发生变化，如果没有发生变化则更新，但是如果一个值原来是 A，变成了 B，又变成了 A，那么使用 CAS 进行检查时会发现它的值没有发生变化，但是实际上却变化了
+ *  2.循环时间长开销大：自旋 CAS 如果长时间不成功，会给 CPU 带来非常大的执行开销
  */
 public class AtomicIntegerArrayTest {
     private static int threadCount = 1000;
