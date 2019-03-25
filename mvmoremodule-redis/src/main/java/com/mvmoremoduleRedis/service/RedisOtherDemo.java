ec356bef39c9de325e9cd00a21a3e3f9ebe73279
xuzhiyong28 <jip0574@qq.com>
2019-03-25 17:48:55 +0800

    xzy_branch_001 update

---
+++ mvmoremodule-redis/src/main/java/com/mvmoremoduleRedis/service/RedisOtherDemo.java
@@ -0,0 +1,31 @@
+package com.mvmoremoduleRedis.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class RedisOtherDemo {
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    public List<String> getValueBySql(String key){
+        System.out.println("这里模拟从数据库中获取数据");
+        return new ArrayList<>();
+    }
+
+    public List<String> getCache(String key){
+        List<String> resultList = (List<String>)redisTemplate.opsForValue().get(key);
+        if(resultList == null || CollectionUtils.isEmpty(resultList)){
+            resultList = getValueBySql(key);
+            redisTemplate.opsForValue().set(key, resultList, 1000, TimeUnit.SECONDS);
+            return resultList;
+        }else{
+            return resultList;
+        }
+    }
+
+}
