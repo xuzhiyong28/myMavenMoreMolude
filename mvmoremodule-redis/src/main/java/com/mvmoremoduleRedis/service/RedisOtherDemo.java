7c1d7b8115dc536e927107235a466cee271ee482
xuzhiyong28 <jip0574@qq.com>
2019-03-25 22:43:56 +0800

    添加新修改的内容

--- mvmoremodule-redis/src/main/java/com/mvmoremoduleRedis/service/RedisOtherDemo.java
+++ mvmoremodule-redis/src/main/java/com/mvmoremoduleRedis/service/RedisOtherDemo.java
@@ -1,6 +1,9 @@
 package com.mvmoremoduleRedis.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.CollectionUtils;
 
@@ -16,7 +19,6 @@ public class RedisOtherDemo {
         System.out.println("这里模拟从数据库中获取数据");
         return new ArrayList<>();
     }
-
     public List<String> getCache(String key){
         List<String> resultList = (List<String>)redisTemplate.opsForValue().get(key);
         if(resultList == null || CollectionUtils.isEmpty(resultList)){
@@ -48,6 +50,29 @@ public class RedisOtherDemo {
         }else{
             return resultList;
         }
+    }
+
+    public List<String> getCacheSave2(String key) throws InterruptedException {
+        List<String> resultList = (List<String>)redisTemplate.opsForValue().get(key);
+        if(CollectionUtils.isEmpty(resultList)){
+            final String mutexKey = key + "_lock";
+            boolean isLock = (Boolean) redisTemplate.execute(new RedisCallback() {
+                @Override
+                public Object doInRedis(RedisConnection connection) throws DataAccessException {
+                    //只在键 key 不存在的情况下， 将键 key 的值设置为 value,若键 key 已经存在， 则 SETNX 命令不做任何动作
+                    //命令在设置成功时返回 1 ， 设置失败时返回 0
+                    return connection.setNX(mutexKey.getBytes(),"1".getBytes());
+                }
+            });
+            if(isLock){
+
+            }
+        }else{
+            //线程休息50毫秒后重试
+            Thread.sleep(50);
+            return getCacheSave2(key);
+        }
+        return resultList;
     }
 
 }
