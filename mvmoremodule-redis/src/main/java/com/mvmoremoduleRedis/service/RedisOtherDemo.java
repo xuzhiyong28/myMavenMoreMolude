926a726d7af1a1997a5086b5c0344d85086a22f9
xuzhiyong28 <jip0574@qq.com>
2019-03-26 15:26:07 +0800

    xzy_branch_001 update

--- mvmoremodule-redis/src/main/java/com/mvmoremoduleRedis/service/RedisOtherDemo.java
+++ mvmoremodule-redis/src/main/java/com/mvmoremoduleRedis/service/RedisOtherDemo.java
@@ -1,23 +1,25 @@
 package com.mvmoremoduleRedis.service;
 
+import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
+@Component("redisOtherDemo")
 public class RedisOtherDemo {
     @Autowired
     private RedisTemplate redisTemplate;
 
+
     public List<String> getValueBySql(String key){
         System.out.println("这里模拟从数据库中获取数据");
-        return new ArrayList<>();
+        return Lists.newArrayList("1","2","3","4");
     }
     public List<String> getCache(String key){
         List<String> resultList = (List<String>)redisTemplate.opsForValue().get(key);
@@ -52,25 +54,44 @@ public class RedisOtherDemo {
         }
     }
 
-    public List<String> getCacheSave2(String key) throws InterruptedException {
+    /***
+     *
+     * @param key
+     * @param retryCount 重试次数
+     * @return
+     * @throws InterruptedException
+     */
+    public List<String> getCacheSave2(String key,int retryCount) throws InterruptedException {
         List<String> resultList = (List<String>)redisTemplate.opsForValue().get(key);
         if(CollectionUtils.isEmpty(resultList)){
             final String mutexKey = key + "_lock";
             boolean isLock = (Boolean) redisTemplate.execute(new RedisCallback() {
                 @Override
                 public Object doInRedis(RedisConnection connection) throws DataAccessException {
-                    //只在键 key 不存在的情况下， 将键 key 的值设置为 value,若键 key 已经存在， 则 SETNX 命令不做任何动作
+                    //只在键key不存在的情况下，将键key的值设置为value,若键key已经存在，则 SETNX 命令不做任何动作
                     //命令在设置成功时返回 1 ， 设置失败时返回 0
                     return connection.setNX(mutexKey.getBytes(),"1".getBytes());
                 }
             });
             if(isLock){
-
+                //设置成1秒过期
+                redisTemplate.expire(mutexKey, 1000, TimeUnit.MILLISECONDS);
+                resultList = getValueBySql(key);
+                redisTemplate.opsForValue().set(key, resultList, 1000, TimeUnit.SECONDS);
+                redisTemplate.delete(mutexKey);
+            }else{
+                //线程休息50毫秒后重试
+                Thread.sleep(50);
+                retryCount--;
+                System.out.println("=====进行重试，当前次数:" + retryCount);
+                if(retryCount == 0){
+                    System.out.println("====这里发邮件或者记录下获取不到数据的日志，并为key设置一个空置防止重复获取");
+                    List<String> list = Lists.newArrayList("no find");
+                    redisTemplate.opsForValue().set(key, list, 1000, TimeUnit.SECONDS);
+                    return list;
+                }
+                return getCacheSave2(key,retryCount);
             }
-        }else{
-            //线程休息50毫秒后重试
-            Thread.sleep(50);
-            return getCacheSave2(key);
         }
         return resultList;
     }
--- mvmoremodule-redis/src/test/java/Test.java
+++ mvmoremodule-redis/src/test/java/Test.java
@@ -2,7 +2,10 @@
  * Created by Administrator on 2018-04-28.
  */
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.mvmoremoduleRedis.service.RedisHyperLogLog;
+import com.mvmoremoduleRedis.service.RedisOtherDemo;
 import org.junit.Before;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
@@ -11,6 +14,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.RedisTemplate;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author xuzhiyong
  * @createDate 2018-04-28-17:11
@@ -18,6 +24,8 @@ import org.springframework.data.redis.core.RedisTemplate;
 public class Test {
 
     public ApplicationContext applicationContext;
+
+    public RedisTemplate redisTemplate;
     @Before
     public void initApp(){
         applicationContext = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
@@ -35,7 +43,29 @@ public class Test {
         DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
         XmlBeanDefinitionReader xmlBeanDefinitionReader = new XmlBeanDefinitionReader(factory);
         xmlBeanDefinitionReader.loadBeanDefinitions(classPathResource);
+    }
 
+    @org.junit.Test
+    public void testRedis1(){
+        redisTemplate = (RedisTemplate)applicationContext.getBean("redisTemplate");
+        JSONObject object = new JSONObject();
+        object.put("name","xuzhiyong");
+        object.put("age","28");
+        redisTemplate.opsForValue().set("xuzy",object,1000, TimeUnit.SECONDS);
+        JSONObject redisObj = (JSONObject) redisTemplate.opsForValue().get("xuzy");
+        System.out.println(redisObj);
+    }
+
+
+    @org.junit.Test
+    public void redisOther1(){
+        RedisOtherDemo redisOtherDemo = (RedisOtherDemo)applicationContext.getBean("redisOtherDemo");
+        String key = "testKey";
+        try {
+            List<String> list = redisOtherDemo.getCacheSave2(key,10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 
