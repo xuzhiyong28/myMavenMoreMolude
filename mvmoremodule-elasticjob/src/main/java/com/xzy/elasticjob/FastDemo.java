1a747a59668ec0e959ca5fdbaa333493b0cabde0
xuzhiyong28 <jip0574@qq.com>
2019-04-24 21:42:23 +0800

    添加新修改的内容

---
+++ mvmoremodule-elasticjob/src/main/java/com/xzy/elasticjob/FastDemo.java
@@ -0,0 +1,36 @@
+package com.xzy.elasticjob;
+
+import com.dangdang.ddframe.job.config.JobCoreConfiguration;
+import com.dangdang.ddframe.job.config.simple.SimpleJobConfiguration;
+import com.dangdang.ddframe.job.lite.api.JobScheduler;
+import com.dangdang.ddframe.job.lite.config.LiteJobConfiguration;
+import com.dangdang.ddframe.job.reg.base.CoordinatorRegistryCenter;
+import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperConfiguration;
+import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperRegistryCenter;
+
+/**
+ * @author xuzhiyong
+ * @createDate 2019-04-24-21:33
+ */
+public class FastDemo {
+    private static CoordinatorRegistryCenter createRegistryCenter(){
+        CoordinatorRegistryCenter registryCenter = new ZookeeperRegistryCenter(new ZookeeperConfiguration("zk_host:2181","elastic-job-demo"));
+        registryCenter.init();
+        return registryCenter;
+    }
+
+    private static LiteJobConfiguration createJobConfiguration() {
+        JobCoreConfiguration simpleCoreConfig = JobCoreConfiguration.newBuilder("demoSimpleJob", "0/15 * * * * ?", 10).build();
+        // 定义SIMPLE类型配置
+        SimpleJobConfiguration simpleJobConfig = new SimpleJobConfiguration(simpleCoreConfig, MyElasticJob.class.getCanonicalName());
+        // 定义Lite作业根配置
+        LiteJobConfiguration simpleJobRootConfig = LiteJobConfiguration.newBuilder(simpleJobConfig).build();
+        return simpleJobRootConfig;
+    }
+
+
+    public static void main(String[] args){
+        new JobScheduler(createRegistryCenter(), createJobConfiguration()).init();
+    }
+}
+
---
+++ mvmoremodule-elasticjob/src/main/java/com/xzy/elasticjob/MyElasticJob.java
@@ -0,0 +1,27 @@
+package com.xzy.elasticjob;
+
+import com.dangdang.ddframe.job.api.ShardingContext;
+import com.dangdang.ddframe.job.api.simple.SimpleJob;
+
+/**
+ * @author xuzhiyong
+ * @createDate 2019-04-24-21:33
+ */
+public class MyElasticJob implements SimpleJob {
+    @Override
+    public void execute(ShardingContext shardingContext) {
+        switch (shardingContext.getShardingItem()){
+            case 0:
+                System.out.println("0");
+                break;
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            default:
+                System.out.println("default");
+        }
+    }
+}
