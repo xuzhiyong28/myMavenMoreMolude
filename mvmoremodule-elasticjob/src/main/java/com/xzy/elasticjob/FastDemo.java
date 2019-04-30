c088890ce01475ca279cc829eba4722c057a06f1
xuzhiyong28 <jip0574@qq.com>
2019-04-30 09:36:42 +0800

    xzy_branch_001 update

--- mvmoremodule-elasticjob/src/main/java/com/xzy/elasticjob/FastDemo.java
+++ mvmoremodule-elasticjob/src/main/java/com/xzy/elasticjob/FastDemo.java
@@ -20,16 +20,21 @@ public class FastDemo {
     }
 
     private static LiteJobConfiguration createJobConfiguration() {
+        //看清楚 JobCoreConfiguration JobTypeConfiguration LiteJobConfiguration 三者的关系
+        //JobCoreConfiguration
         JobCoreConfiguration simpleCoreConfig = JobCoreConfiguration
                 .newBuilder("demoSimpleJob", "0/20 * * * * ?", 4)
-                .shardingItemParameters("0=A,1=B,2=C,3=D")
+                .shardingItemParameters("0=A,1=B,2=C,3=D") //分片以及对应的参数，分片序列号从0开始，大于总片数要报错
                 .jobParameter("xuzy")
-                .failover(true) //设置失效转移
+                .misfire(true) //是否开启错过任务重新执行 默认是true
+                .failover(true) //设置失效转移 开启表示如果作业在一次任务执行中途宕机允许将该次未完成的任务在另一作业节点上补偿执行
                 .build();
-        // 定义SIMPLE类型配置
+        // 定义SIMPLE类型配置 JobTypeConfiguration
         SimpleJobConfiguration simpleJobConfig = new SimpleJobConfiguration(simpleCoreConfig, MyElasticJob.class.getCanonicalName());
-        // 定义Lite作业根配置
-        LiteJobConfiguration simpleJobRootConfig = LiteJobConfiguration.newBuilder(simpleJobConfig).build();
+        // 定义Lite作业根配置 LiteJobConfiguration
+        LiteJobConfiguration simpleJobRootConfig = LiteJobConfiguration.newBuilder(simpleJobConfig)
+                //.jobShardingStrategyClass(MyJobShardingStrategy.class.getName()) 自定义分片策略
+                .build();
         return simpleJobRootConfig;
     }
 
