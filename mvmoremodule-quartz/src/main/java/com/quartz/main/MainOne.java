cdcaf2f38210e53b38b9ca57c178ae298fc3a18e
xuzhiyong28 <jip0574@qq.com>
2019-05-06 22:59:31 +0800

    添加新修改的内容

--- mvmoremodule-quartz/src/main/java/com/quartz/main/MainOne.java
+++ mvmoremodule-quartz/src/main/java/com/quartz/main/MainOne.java
@@ -6,7 +6,7 @@ import org.quartz.impl.StdSchedulerFactory;
 
 public class MainOne {
     public static void main(String[] args) throws SchedulerException {
-        test1();
+        test2();
     }
 
     public static void test1() throws SchedulerException {
@@ -17,6 +17,17 @@ public class MainOne {
         Scheduler scheduler = schedulerFactory.getScheduler();
         scheduler.scheduleJob(jobDetail, trigger);
         scheduler.start();
+    }
+
+    public static void test2() throws SchedulerException {
+        JobDetail jobDetail = JobBuilder.newJob(JobOne.class).withIdentity("jobOne", "jGroup1").build();
+        CronScheduleBuilder cronScheduleBuilder = CronScheduleBuilder.cronSchedule("0/5 * * * * ?");
+        Trigger trigger = TriggerBuilder.newTrigger().withIdentity("trigger_1", "jGroup1").withSchedule(cronScheduleBuilder).build();
+        SchedulerFactory schedulerFactory = new StdSchedulerFactory();
+        Scheduler scheduler = schedulerFactory.getScheduler();
+        scheduler.scheduleJob(jobDetail, trigger);
+        scheduler.start();
+
     }
 
 }
