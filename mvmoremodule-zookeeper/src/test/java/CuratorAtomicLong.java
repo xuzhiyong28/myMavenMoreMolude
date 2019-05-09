daba3fc60596dafcb9c3b83555362a8b07b92bd9
xuzhiyong28 <jip0574@qq.com>
2019-05-09 14:59:39 +0800

    xzy_branch_001 update

--- mvmoremodule-zookeeper/src/test/java/CuratorAtomicLong.java
+++ mvmoremodule-zookeeper/src/test/java/CuratorAtomicLong.java
@@ -29,7 +29,7 @@ public class CuratorAtomicLong {
 
     //你必须检查返回结果的succeeded()， 它代表此操作是否成功。 如果操作成功， preValue()代表操作前的值， postValue()代表操作后的值
     @Test
-    public void test() throws InterruptedException {
+    public void test() throws Exception {
         List<DistributedAtomicLong> examples = Lists.newArrayList();
         CuratorFramework cf = CuratorFrameworkFactory.newClient("localhost:2181", new ExponentialBackoffRetry(2000, 3));
         cf.start();
@@ -54,6 +54,47 @@ public class CuratorAtomicLong {
         service.shutdown();
         service.awaitTermination(10, TimeUnit.MINUTES);
         TimeUnit.SECONDS.sleep(60);
+        DistributedAtomicLong currentCount = new DistributedAtomicLong(cf,PATH,new RetryNTimes(10,100));
+        System.out.println(currentCount.get().postValue());
         System.out.println("job end!!!");
     }
+
+    @Test
+    public void test2() throws Exception {
+        List<DistributedAtomicLong> examples = Lists.newArrayList();
+        CuratorFramework cf = CuratorFrameworkFactory.newClient("localhost:2181", new ExponentialBackoffRetry(2000, 3));
+        cf.start();
+        ExecutorService service = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < QTY; ++i) {
+            final int index = i;
+            final DistributedAtomicLong count = new DistributedAtomicLong(cf,PATH,new RetryNTimes(10,100));
+            examples.add(count);
+            Callable<Void> task = new Callable<Void>() {
+                @Override
+                public Void call() throws Exception {
+                    //进行重复加1 如果设置不成功重新进行设置
+                    AtomicValue<Long> value = count.increment(); //+1
+                    while(value.succeeded() != true){
+                        value = count.increment();
+                        if(value.succeeded()){
+                            System.out.println("Increment: from " + value.preValue() + " to " + value.postValue());
+                            break;
+                        }
+                    }
+                    return null;
+                }
+            };
+            service.submit(task);
+        }
+        service.shutdown();
+        service.awaitTermination(10, TimeUnit.MINUTES);
+        TimeUnit.SECONDS.sleep(60);
+        DistributedAtomicLong currentCount = new DistributedAtomicLong(cf,PATH,new RetryNTimes(10,100));
+        System.out.println(currentCount.get().postValue());
+        System.out.println("job end!!!");
+    }
+
+
+
+
 }
