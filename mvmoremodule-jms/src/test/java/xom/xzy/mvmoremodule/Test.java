8ad94a03ade75d1b991f3013fbc1eef4ab57ec73
xuzhiyong28 <jip0574@qq.com>
2019-05-30 19:41:05 +0800

    xzy_branch_001 update

--- mvmoremodule-jms/src/test/java/xom/xzy/mvmoremodule/Test.java
+++ mvmoremodule-jms/src/test/java/xom/xzy/mvmoremodule/Test.java
@@ -2,6 +2,7 @@ package xom.xzy.mvmoremodule;/**
  * Created by Administrator on 2018-03-30.
  */
 
+import org.apache.commons.lang3.RandomUtils;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.core.io.ClassPathResource;
@@ -24,7 +25,8 @@ public class Test {
 
     @org.junit.Test
     public void test2(){
-        Random r = new Random();
-        System.out.println((r.nextInt(10) + 1) * 1000);
+        for(int i = 1 ; i <= 288; i++){
+            System.out.println(RandomUtils.nextInt(10000000,50000000));
+        }
     }
 }
