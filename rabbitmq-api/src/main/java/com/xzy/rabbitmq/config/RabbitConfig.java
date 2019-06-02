82dbf8e9a57717f4e2597498bff92252380fc208
xuzhiyong28 <jip0574@qq.com>
2019-06-02 22:27:04 +0800

    添加新修改的内容

--- rabbitmq-api/src/main/java/com/xzy/rabbitmq/config/RabbitConfig.java
+++ rabbitmq-api/src/main/java/com/xzy/rabbitmq/config/RabbitConfig.java
@@ -22,8 +22,8 @@ public class RabbitConfig {
     @Bean
     public ConnectionFactory connectionFactory(){
         CachingConnectionFactory connectionFactory = new CachingConnectionFactory("127.0.0.1");
-        connectionFactory.setUsername("wsz");
-        connectionFactory.setPassword("123");
+        connectionFactory.setUsername("guest");
+        connectionFactory.setPassword("guest");
         connectionFactory.setPort(5672);
         return connectionFactory;
     }
