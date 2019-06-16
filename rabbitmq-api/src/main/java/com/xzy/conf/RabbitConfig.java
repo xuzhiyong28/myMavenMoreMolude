e925083242a54fe9a55fd8dd4340a67a050fa9d4
xuzhiyong28 <jip0574@qq.com>
2019-06-16 15:08:28 +0800

    添加新修改的内容

---
+++ rabbitmq-api/src/main/java/com/xzy/conf/RabbitConfig.java
@@ -0,0 +1,99 @@
+package com.xzy.conf;
+
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.AcknowledgeMode;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.ChannelAwareMessageListener;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.support.ConsumerTagStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.UUID;
+
+/**
+ * @author xuzhiyong
+ * @createDate 2019-06-15-11:32
+ */
+@Configuration
+@EnableRabbit
+public class RabbitConfig {
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory("127.0.0.1");
+        connectionFactory.setUsername("guest");
+        connectionFactory.setPassword("guest");
+        connectionFactory.setPort(5672);
+        connectionFactory.setVirtualHost("/");
+        return connectionFactory;
+    }
+
+    @Bean
+    public RabbitAdmin amqpAdmin() {
+        return new RabbitAdmin(connectionFactory());
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory());
+        return rabbitTemplate;
+    }
+
+    //配置消费者监听的容器
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory() {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory());
+        factory.setConcurrentConsumers(3);
+        factory.setMaxConcurrentConsumers(10);
+        return factory;
+    }
+
+
+    //简单消息监听容器
+    @Bean
+    public SimpleMessageListenerContainer simpleMessageListenerContainer(ConnectionFactory connectionFactory) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer(connectionFactory);
+        //设置需要监听的队列
+        container.setQueueNames("test.direct.queue", "test.topic.queue", "test.fanout.queue");
+        //当前的消费者数量
+        container.setConcurrentConsumers(1);
+        //最大的消费者数量
+        container.setMaxConcurrentConsumers(5);
+        container.setDefaultRequeueRejected(false);
+        //设置自动签收
+        container.setAcknowledgeMode(AcknowledgeMode.AUTO);
+        container.setConsumerTagStrategy(new ConsumerTagStrategy() {
+            @Override
+            public String createConsumerTag(String queue) {
+                return queue + "_" + UUID.randomUUID().toString();
+            }
+        });
+        //设置消息监听
+        container.setMessageListener(new ChannelAwareMessageListener() {
+            @Override
+            public void onMessage(Message message, Channel channel) throws Exception {
+                String msg = new String(message.getBody());
+                System.out.println("-----消费者:" + msg);
+            }
+        });
+        container.setConsumerTagStrategy(new ConsumerTagStrategy() {
+            @Override
+            public String createConsumerTag(String queue) {
+                return queue + "_" + UUID.randomUUID().toString();
+            }
+        });
+        return container;
+    }
+
+
+
+
+}
--- rabbitmq-api/src/test/SrpingTest.java
+++ rabbitmq-api/src/test/SrpingTest.java
@@ -1,25 +1,80 @@
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.HashMap;
+
 @RunWith(SpringJUnit4ClassRunner.class)     //表示继承了SpringJUnit4ClassRunner类
-@ContextConfiguration(locations = { "classpath:applicationContext.xml" })
+@ContextConfiguration(locations = {"classpath:applicationContext.xml"})
 public class SrpingTest {
 
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
+    @Autowired
+    private RabbitAdmin rabbitAdmin;
+
 
     @Test
-    public void test(){
+    public void initQueue() {
+
+        //声明direct类型交换机和队列
+        rabbitAdmin.declareExchange(new DirectExchange("test.direct", false, false));
+        rabbitAdmin.declareQueue(new Queue("test.direct.queue", false));
+        rabbitAdmin.declareBinding(new Binding("test.direct.queue",
+                Binding.DestinationType.QUEUE, "test.direct", "directKey", new HashMap<String, Object>()));
+
+
+        //声明topic类型交换机和队列
+        rabbitAdmin.declareExchange(new DirectExchange("test.topic", false, false));
+        rabbitAdmin.declareQueue(new Queue("test.topic.queue", false));
+        rabbitAdmin.declareBinding(new Binding("test.topic.queue",
+                Binding.DestinationType.QUEUE, "test.topic", "topicKey.*", new HashMap<String, Object>()));
+
+
+        //声明fanout类型的交换机和队列
+        rabbitAdmin.declareExchange(new FanoutExchange("test.fanout", false, false));
+        rabbitAdmin.declareQueue(new Queue("test.fanout.queue", false));
+        rabbitAdmin.declareBinding(new Binding("test.fanout.queue",
+                Binding.DestinationType.QUEUE, "test.fanout", "", new HashMap<String, Object>()));
+
 
     }
+
+
+    @Test
+    public void testSendMessage() {
+        //创建消息
+        MessageProperties messageProperties = new MessageProperties();
+        messageProperties.getHeaders().put("desc", "信息描述..");
+        messageProperties.getHeaders().put("type", "自定义消息类型..");
+        Message message = new Message("Hello RabbitMQ".getBytes(), messageProperties);
+        rabbitTemplate.convertAndSend("test.direct", "directKey", message, new MessagePostProcessor() {
+            @Override
+            public Message postProcessMessage(Message message) throws AmqpException {
+                System.err.println("------添加额外的设置---------");
+                message.getMessageProperties().getHeaders().put("desc", "额外修改的信息描述");
+                message.getMessageProperties().getHeaders().put("attr", "额外新加的属性");
+                return message;
+            }
+        });
+    }
+
+    @Test
+    public void testSendMessage2(){
+        MessageProperties messageProperties = new MessageProperties();
+        messageProperties.setContentType("text/plan");
+        Message message = new Message("mg 消息1234".getBytes() , messageProperties);
+        rabbitTemplate.send("test.direct","directKey",message);
+        rabbitTemplate.convertAndSend("test.direct","directKey","message other");
+    }
+
+
+    
 }
