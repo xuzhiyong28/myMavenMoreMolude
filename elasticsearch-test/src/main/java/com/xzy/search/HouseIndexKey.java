033233f5db502a45941e3fbe9baffe93d6631b52
xuzhiyong28 <jip0574@qq.com>
2019-10-03 20:30:58 +0800

    update by xuzy

---
+++ elasticsearch-test/src/main/java/com/xzy/search/HouseIndexKey.java
@@ -0,0 +1,30 @@
+package com.xzy.search;
+
+/***
+ * 索引关键词
+ */
+public class HouseIndexKey {
+    public static final String HOUSE_ID = "houseid";
+    public static final String TITLE = "title";
+    public static final String PRICE = "price";
+    public static final String AREA = "area";
+    public static final String CREATETIME = "createTime";
+    public static final String LASTUPDATETIME = "lastUpdateTime";
+    public static final String CITYENNAME = "cityEnName";
+    public static final String REGIONENNAME = "regionEnName";
+    public static final String DIRECTION = "direction";
+    public static final String DISTANCETOSUBWAY = "distanceToSubway";
+    public static final String SUBWAYLINENAME = "subwayLineName";
+    public static final String SUBWAYSTATIONNAME = "subwayStationName";
+    public static final String TAGS = "tags";
+    public static final String STREET = "street";
+    public static final String DISTRICT = "district";
+    public static final String DESCRIPTION = "description";
+    public static final String LAYOUTDESC = "layoutDesc";
+    public static final String TRAFFIC = "traffic";
+    public static final String ROUNDSERVICE = "roundService";
+    public static final String RENTWAY = "rentWay";
+
+
+
+}
