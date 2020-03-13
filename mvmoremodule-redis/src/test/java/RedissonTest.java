6abddf68124e1d3761cf8af074eb2c7390485522
xuzhiyong28 <jip0574@qq.com>
2020-03-13 09:27:16 +0800

    update by xuzy

--- mvmoremodule-redis/src/test/java/RedissonTest.java
+++ mvmoremodule-redis/src/test/java/RedissonTest.java
@@ -1,13 +1,18 @@
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import org.junit.Assert;
 import org.junit.Test;
 import org.redisson.Redisson;
+import org.redisson.api.RAtomicLong;
+import org.redisson.api.RFuture;
 import org.redisson.api.RLock;
 import org.redisson.config.Config;
 
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class RedissonTest {
@@ -17,63 +22,64 @@ public class RedissonTest {
 
     static {
         config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+        config.setLockWatchdogTimeout(10 * 1000);
         redisson = (Redisson) Redisson.create(config);
     }
 
 
     @Test
-    public void acquireTest() {
-        System.out.println(acquire("xuzyLock"));
-        System.out.println(acquire("xuzyLock"));
+    public void test0(){
     }
+
+
+    @Test
+    public void test1() {
+        RAtomicLong longObject = redisson.getAtomicLong("myLong");
+        boolean b = longObject.compareAndSet(0, 400);
+        Assert.assertEquals(b, true);
+        longObject.getAndIncrement();
+        Assert.assertEquals(longObject.get(), 401);
+        longObject.delete();
+    }
+
+    @Test
+    public void test2() throws ExecutionException, InterruptedException {
+        RAtomicLong longObject = redisson.getAtomicLong("myLong");
+        RFuture<Boolean> booleanRFuture = longObject.compareAndSetAsync(0, 400);
+        booleanRFuture.whenCompleteAsync((aBoolean, throwable) -> {
+            System.out.println("===whenCompleteAsync is complete===");
+        }, Executors.newCachedThreadPool());
+        booleanRFuture.get();
+        TimeUnit.SECONDS.sleep(1);
+    }
+
+
+    @Test
+    public void test3(){
+        String key = LOCK_TITLE + "_lock";
+        RLock mylock = redisson.getLock(key);
+        mylock.lock();
+        mylock.lock();
+    }
+
+
 
 
     @Test
     public void testLock() throws InterruptedException {
         String key = LOCK_TITLE + "_lock";
         RLock mylock = redisson.getLock(key);
-        Map<String,String> myMap = Maps.newHashMap();
+        Map<String, String> myMap = Maps.newHashMap();
         List<Thread> threadList = Lists.newArrayList();
         for (int i = 0; i < 1000; i++) {
-            Thread thread = new Thread(new Runner(mylock,myMap));
+            Thread thread = new Thread(new Runner(mylock, myMap));
             thread.start();
             threadList.add(thread);
         }
-        for(Thread thread : threadList){
+        for (Thread thread : threadList) {
             thread.join();
         }
         System.out.println(myMap.entrySet().size());
-    }
-
-
-    /***
-     * 加锁
-     * @param lockName
-     * @return
-     */
-    public static boolean acquire(String lockName) {
-        String key = LOCK_TITLE + lockName;
-        //获取锁对象
-        RLock mylock = redisson.getLock(key);
-        //加锁，并且设置锁过期时间，防止死锁的产生
-        try {
-            mylock.lock(10, TimeUnit.SECONDS);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-        return true;
-    }
-
-    /***
-     * 解锁
-     * @param lockName
-     */
-    public static void release(String lockName) {
-        String key = LOCK_TITLE + lockName;
-        //获取锁对象
-        RLock mylock = redisson.getLock(key);
-        mylock.unlock();
     }
 
 
