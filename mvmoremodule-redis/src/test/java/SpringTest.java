913f7a2ade8d3f34c2e3da1a2c776c52a7fa5e7b
xuzhiyong28 <jip0574@qq.com>
2020-05-27 14:18:24 +0800

    update by xuzy

--- mvmoremodule-redis/src/test/java/SpringTest.java
+++ mvmoremodule-redis/src/test/java/SpringTest.java
@@ -1,6 +1,15 @@
 import com.springTest.AppConfig;
 import com.springTest.BeanA;
+import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.io.ClassRelativeResourceLoader;
+import org.springframework.core.io.FileSystemResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * @author xuzhiyong
@@ -12,5 +21,37 @@ public class SpringTest {
         AnnotationConfigApplicationContext annotationConfigApplicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         System.out.println(annotationConfigApplicationContext.getBean(BeanA.class));
+    }
+
+    /***
+     * spring 资源加载器
+     * @throws IOException
+     */
+    @Test
+    public void resourceTest() throws IOException {
+        ResourceLoader classResourceLoader = new ClassRelativeResourceLoader(BeanA.class);
+        Resource resource = classResourceLoader.getResource("AppConfig.class");
+        System.out.println(resource.getFile().getPath());
+
+        ResourceLoader fileResourceLoader = new FileSystemResourceLoader();
+        Resource fileResource = fileResourceLoader.getResource("D:\\workspack\\myMavenMoreMolude\\mvmoremodule-redis\\src\\main\\resources\\test.lua");
+        System.out.println(fileResource);
+
+        //这种最好用，支持通配符
+        //classpath: 用于加载类路径（包括jar包）中的一个且仅一个资源
+        //classpath*: 用于加载类路径（包括jar包）中的所有匹配的资源
+        PathMatchingResourcePatternResolver pathMatchingResourceLoader = new PathMatchingResourcePatternResolver();
+        Resource resource1 = pathMatchingResourceLoader.getResource("classpath:bean.xml");
+        System.out.println(String.format("资源路径= %s", resource1.getFile().getPath()));
+        Resource resource2 = pathMatchingResourceLoader.getResource("classpath:com/springTest/BeanA.class");
+        System.out.println(String.format("资源路径= %s", resource2.getFile().getPath()));
+        Resource[] resource3 = pathMatchingResourceLoader.getResources("classpath*:com/*/*.class");
+        Arrays.stream(resource3).forEach(resource4 -> {
+            try {
+                System.out.println(String.format("资源路径= %s", resource4.getFile().getPath()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
