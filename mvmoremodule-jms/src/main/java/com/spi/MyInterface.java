2d5aa31e75350cf463f4d73ff9162394b04cfeba
xuzhiyong28 <jip0574@qq.com>
2020-06-24 15:13:12 +0800

    update by xuzy

---
+++ mvmoremodule-jms/src/main/java/com/spi/MyInterface.java
@@ -0,0 +1,5 @@
+package com.spi;
+
+public interface MyInterface {
+    void shout();
+}
---
+++ mvmoremodule-jms/src/main/java/com/spi/impl/MyInterfaceImplA.java
@@ -0,0 +1,10 @@
+package com.spi.impl;
+
+import com.spi.MyInterface;
+
+public class MyInterfaceImplA implements MyInterface {
+    @Override
+    public void shout() {
+        System.out.println("===========MyInterfaceImplA=============");
+    }
+}
