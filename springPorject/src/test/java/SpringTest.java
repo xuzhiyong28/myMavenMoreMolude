0b6be749b667f66c4392237b5beaf897325a7c37
xuzhiyong28 <jip0574@qq.com>
2020-08-03 09:32:40 +0800

    update by xuzy

--- springPorject/src/test/java/SpringTest.java
+++ springPorject/src/test/java/SpringTest.java
@@ -2,6 +2,7 @@ import com.xzy.annotation.AppConfig;
 import com.xzy.annotation.bean.BeanD;
 import com.xzy.aop.OrderService;
 import com.xzy.aop.UserService;
+import com.xzy.bean.BeanB;
 import com.xzy.bean.BeanC;
 import com.xzy.bean.SpringCycleBean;
 import com.xzy.listener.MyApplicationEvent;
@@ -9,6 +10,7 @@ import com.xzy.postProcessor.MyInstantiationAwareBeanPostProcessor;
 import com.xzy.rwDynamic.DBContextHolder;
 import com.xzy.rwDynamic.DbType;
 import com.xzy.tx.TxService;
+import org.apache.commons.lang3.RandomUtils;
 import org.junit.Test;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
@@ -25,10 +27,11 @@ import org.springframework.transaction.support.DefaultTransactionDefinition;
 import javax.sql.DataSource;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 public class SpringTest {
     @Test
-    public void test1(){
+    public void test1() {
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
         System.out.println("context 启动成功");
         SpringCycleBean bean = context.getBean(SpringCycleBean.class);
@@ -36,7 +39,7 @@ public class SpringTest {
     }
 
     @Test
-    public void testAnnotataion(){
+    public void testAnnotataion() {
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         BeanD bean = context.getBean(BeanD.class);
         context.getBean(SpringCycleBean.class);
@@ -48,7 +51,7 @@ public class SpringTest {
      * spring 加载bean
      */
     @Test
-    public void test2(){
+    public void test2() {
         //获取资源
         ClassPathResource resource = new ClassPathResource("applicationContext.xml");
         //获取BeanFactory
@@ -59,12 +62,12 @@ public class SpringTest {
         XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
         //解析资源
         reader.loadBeanDefinitions(resource);
-        SpringCycleBean bean = factory.getBean(SpringCycleBean.class);
+        BeanB bean = factory.getBean(BeanB.class);
         System.out.println(bean);
     }
 
     @Test
-    public void test3(){
+    public void test3() {
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
         System.out.println("context 启动成功");
         BeanC bean = context.getBean(BeanC.class);
@@ -73,7 +76,7 @@ public class SpringTest {
 
 
     @Test
-    public void testAop(){
+    public void testAop() {
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext-aop.xml");
         UserService userService = context.getBean(UserService.class);
         OrderService orderService = context.getBean(OrderService.class);
@@ -84,7 +87,7 @@ public class SpringTest {
     }
 
     @Test
-    public void testAopAnnotation(){
+    public void testAopAnnotation() {
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext-aop-annotation.xml");
         UserService userService = context.getBean(UserService.class);
         userService.queryUser();
@@ -92,7 +95,7 @@ public class SpringTest {
 
 
     @Test
-    public void testAopConfig(){
+    public void testAopConfig() {
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext-aop-config.xml");
         UserService userService = context.getBean(UserService.class);
         OrderService orderService = context.getBean(OrderService.class);
@@ -103,14 +106,14 @@ public class SpringTest {
     }
 
     @Test
-    public void testTx(){
+    public void testTx() {
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext-tx.xml");
         TxService txService = context.getBean(TxService.class);
         txService.updateData();
     }
 
     @Test
-    public void testTx2(){
+    public void testTx2() {
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext-tx.xml");
         TxService txService = context.getBean(TxService.class);
         txService.updateByThread();
@@ -118,7 +121,7 @@ public class SpringTest {
 
 
     @Test
-    public void testJavaTx(){
+    public void testJavaTx() {
         DataSource dataSource = TestUtils.getDataSource();
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
         //创建物管理器
@@ -129,12 +132,12 @@ public class SpringTest {
         txDefinition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
         //开启事务
         TransactionStatus txStatus = txManager.getTransaction(txDefinition);
-        try{
-            jdbcTemplate.update("INSERT INTO `book`( `book`, `price`) VALUES (?,?)","书",12);
+        try {
+            jdbcTemplate.update("INSERT INTO `book`( `book`, `price`) VALUES (?,?)", "书", 12);
             jdbcTemplate.update("INSERT INTO `user`( `name`, `age`) VALUES (?,?)", "名字1", 12);
             int i = 1 / 0;
             txManager.commit(txStatus);
-        }catch (Exception e){
+        } catch (Exception e) {
             txManager.rollback(txStatus);
             e.printStackTrace();
         }
@@ -142,17 +145,32 @@ public class SpringTest {
 
 
     @Test
-    public void testJdbcTempleThread(){
+    public void testJdbcTempleThread() {
         DataSource dataSource = TestUtils.getDataSource();
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-        List<Map<String, Object>> maps = jdbcTemplate.queryForList("select * from user");
-        List<Map<String, Object>> maps2 = jdbcTemplate.queryForList("select * from user");
-        System.out.println("!!!");
+        //List<Map<String, Object>> maps = jdbcTemplate.queryForList("select * from user");
+        //List<Map<String, Object>> maps2 = jdbcTemplate.queryForList("select * from user");
+        for (int i = 0; i < 10000; i++) {
+            jdbcTemplate.update("INSERT INTO `single_table` (`key1`, `key2`, `key3`, `key_part1`, `key_part2`, `key_part3`, `common_field`) VALUES (?,?,?,?,?,?,?)"
+                    , getRandomString(3), i, getRandomString(4), getRandomString(5), getRandomString(5), getRandomString(5), getRandomString(10));
+        }
     }
 
 
+    //length用户要求产生字符串的长度
+    public static String getRandomString(int length){
+        String str="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        Random random=new Random();
+        StringBuffer sb=new StringBuffer();
+        for(int i=0;i<length;i++){
+            int number=random.nextInt(62);
+            sb.append(str.charAt(number));
+        }
+        return sb.toString();
+    }
+
     @Test
-    public void listenerTest(){
+    public void listenerTest() {
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext-listener.xml");
         //创建自定义的事件
         MyApplicationEvent myApplicationEvent = new MyApplicationEvent("test event!");
@@ -165,7 +183,7 @@ public class SpringTest {
      * 读写分离 动态数据库
      */
     @Test
-    public void testwrDynamic(){
+    public void testwrDynamic() {
         //设置当前线程使用主线程
         DBContextHolder.set(DbType.MASTER.getType());
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext-w-r-dynamic.xml");
