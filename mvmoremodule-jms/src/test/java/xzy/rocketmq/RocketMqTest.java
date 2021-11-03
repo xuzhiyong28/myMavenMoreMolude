f023ce162b7e4341b4909c8bb65612ba1e7071ce
xuzhiyong <xuzhiyong28>
2021-11-03 09:10:05 +0800

    提交提交提交提交提交提交

--- mvmoremodule-jms/src/test/java/xzy/rocketmq/RocketMqTest.java
+++ mvmoremodule-jms/src/test/java/xzy/rocketmq/RocketMqTest.java
@@ -1,8 +1,8 @@
 package xzy.rocketmq;
 
+import com.google.common.collect.Sets;
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
 import org.apache.rocketmq.client.consumer.listener.*;
-import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.client.producer.MessageQueueSelector;
 import org.apache.rocketmq.client.producer.SendCallback;
@@ -13,14 +13,14 @@ import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.common.message.MessageExt;
 import org.apache.rocketmq.common.message.MessageQueue;
 import org.apache.rocketmq.remoting.common.RemotingHelper;
-import org.aspectj.weaver.ast.Or;
 import org.junit.Test;
+import org.testng.collections.Lists;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Random;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class RocketMqTest {
@@ -31,15 +31,15 @@ public class RocketMqTest {
     @Test
     public void syncProducerTest() throws Exception {
         // 实例化消息生产者Producer
-        DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
+        DefaultMQProducer producer = new DefaultMQProducer("syncProducer_1");
         // 设置NameServer的地址
-        producer.setNamesrvAddr("localhost:9876");
+        producer.setNamesrvAddr("192.168.50.234:9876");
         // 启动
         producer.start();
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 800; i++) {
             //创建消息 并指定topic tag 和 消息体
-            Message msg = new Message("TopicTest", "TagA", ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
+            Message msg = new Message("topic_2", null, ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
             // 发送消息
             SendResult sendResult = producer.send(msg);
             System.out.printf("%s%n", sendResult);
@@ -122,30 +122,41 @@ public class RocketMqTest {
     @Test
     public void consumerTest() throws Exception {
         // 实例化消费者
-        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("please_rename_unique_group_name");
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("consumerTest_2");
         // 设置NameServer的地址
-        consumer.setNamesrvAddr("localhost:9876");
+        consumer.setNamesrvAddr("192.168.50.234:9876");
         // 订阅一个或者多个Topic，以及Tag来过滤需要消费的消息
-        consumer.subscribe("TopicTest", "*");
-        // 注册回调实现类来处理从broker拉取回来的消息
+        consumer.subscribe("topic_2", "*");
+        // 设置从queue的第一条消息开始消费
+        // CONSUME_FROM_LAST_OFFSET  从queue的当前最后一条消息开始消费
+        // CONSUME_FROM_FIRST_OFFSET  从queue的第一条消息开始消费
+        // CONSUME_FROM_TIMESTAMP 从指定的具体时间戳位置的消息开始消费。这个具体时间戳 是通过另外一个语句指定的 consumer.setConsumeTimestamp(“20210701080000”) yyyyMMddHHmmss
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+        // 批量消费，每次批量消费10条，最多不能超过32条。List<MessageExt> msgs的值就是10条
+        consumer.setConsumeMessageBatchMaxSize(10);
+        // 注册回调实现类来处理从broker拉取回来的消息 MessageListenerConcurrently表示并发消费，代码里面会启多个线程获取数据
         consumer.registerMessageListener(new MessageListenerConcurrently() {
             @Override
             public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext consumeConcurrentlyContext) {
-                System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msgs);
+                Set<Integer> queueids = Sets.newHashSet();
+                for(MessageExt msg : msgs) {
+                    queueids.add(msg.getQueueId());
+                }
+                System.out.println("当前线程 :" + Thread.currentThread().getName() + ", msgsSize :" + msgs.size() + ", queueids : " + queueids);
                 // 标记该消息已经被成功消费
                 return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
             }
         });
         consumer.start(); // 启动实例
         System.out.printf("Consumer Started.%n");
-        TimeUnit.SECONDS.sleep(10);
+        TimeUnit.SECONDS.sleep(1000);
     }
 
 
     @Test
     public void producerInOrderTest() throws Exception {
-        DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
-        producer.setNamesrvAddr("127.0.0.1:9876");
+        DefaultMQProducer producer = new DefaultMQProducer("producerInOrder");
+        producer.setNamesrvAddr("192.168.50.234:9876");
         producer.start();
         String[] tags = new String[]{"TagA", "TagC", "TagD"};
         List<OrderStep> orderList = buildOrders();
@@ -153,8 +164,8 @@ public class RocketMqTest {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String dateStr = sdf.format(date);
         for (int i = 0; i < 10; i++) {
-            String body = dateStr + " Hello RocketMQ " + orderList.get(i);
-            Message msg = new Message("TopicTest", tags[i % tags.length], "KEY" + i, body.getBytes());
+            String body = dateStr + " Hello RocketMQ " + orderList.get(i).toString();
+            Message msg = new Message("topic_1", tags[i % tags.length], "KEY" + i, body.getBytes());
             SendResult sendResult = producer.send(msg, new MessageQueueSelector() {
                 @Override
                 public MessageQueue select(List<MessageQueue> mqs, Message message, Object arg) {
@@ -173,17 +184,15 @@ public class RocketMqTest {
 
     @Test
     public void consumerInOrderTest() throws Exception {
-        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("please_rename_unique_group_name_3");
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("consumerInOrder001");
         consumer.setNamesrvAddr("127.0.0.1:9876");
         /**
          * 设置Consumer第一次启动是从队列头部开始消费还是队列尾部开始消费<br>
          * 如果非第一次启动，那么按照上次消费的位置继续消费
          */
         consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
-        consumer.subscribe("TopicTest", "TagA || TagC || TagD");
+        consumer.subscribe("TopicTest", "*");
         consumer.registerMessageListener(new MessageListenerOrderly() {
-            Random random = new Random();
-
             @Override
             public ConsumeOrderlyStatus consumeMessage(List<MessageExt> msgs, ConsumeOrderlyContext consumeOrderlyContext) {
                 consumeOrderlyContext.setAutoCommit(true);
@@ -193,7 +202,7 @@ public class RocketMqTest {
                 }
                 try {
                     //模拟业务逻辑处理中...
-                    TimeUnit.SECONDS.sleep(random.nextInt(10));
+                    TimeUnit.SECONDS.sleep(2);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -202,7 +211,61 @@ public class RocketMqTest {
         });
         consumer.start();
         System.out.println("Consumer Started.");
-        TimeUnit.SECONDS.sleep(100);
+        TimeUnit.SECONDS.sleep(1000);
+    }
+
+
+    /***
+     * 延时消息
+     */
+    @Test
+    public void scheduledMessageProducerTest() throws Exception {
+        DefaultMQProducer producer = new DefaultMQProducer("scheduledMessageProducer01");
+        producer.setNamesrvAddr("127.0.0.1:9876");
+        producer.start();
+        int totalMessagesToSend = 100;
+        for (int i = 0; i < totalMessagesToSend; i++) {
+            Message message = new Message("TestTopic_delay", ("Hello scheduled message " + i).getBytes());
+            // 设置延时等级3,这个消息将在10s之后发送(现在只支持固定的几个时间,详看delayTimeLevel)
+            message.setDelayTimeLevel(3);
+            producer.send(message);
+        }
+        producer.shutdown();
+    }
+
+    @Test
+    public void scheduledMessageConsumerTest() throws Exception{
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("scheduledMessageConsumer_01");
+        consumer.subscribe("TestTopic_delay","*");
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> messages, ConsumeConcurrentlyContext context) {
+                for (MessageExt message : messages) {
+                    System.out.println("Receive message[msgId=" + message.getMsgId() + "] " + (System.currentTimeMillis() - message.getBornTimestamp()) + "ms later");
+                }
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        consumer.start();
+    }
+
+
+    /***
+     * 批量发送消息
+     */
+    @Test
+    public void producerBatchTest() throws Exception{
+        DefaultMQProducer producer = new DefaultMQProducer("producerBatch01");
+        producer.setNamesrvAddr("127.0.0.1:9876");
+        producer.start();
+        String topic = "Topic_batch";
+        List<Message> messages = Lists.newArrayList();
+        messages.add(new Message(topic, "TagA", "OrderID001", "Hello world 0".getBytes()));
+        messages.add(new Message(topic, "TagA", "OrderID002", "Hello world 1".getBytes()));
+        messages.add(new Message(topic, "TagA", "OrderID003", "Hello world 2".getBytes()));
+        producer.send(messages);
+        producer.shutdown();
     }
 
 
